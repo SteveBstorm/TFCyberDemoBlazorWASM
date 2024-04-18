a34de3a95268ff250430fad6b75dee9b92c517dd
stevebstorm <steve.lorent@gmail.Com>
2024-04-18 16:37:11 +0200

    Formulaire

---
+++ DemoBlazorWASM/Pages/Demo3/Demo3.razor.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace DemoBlazorWASM.Pages.Demo3
+{
+    public partial class Demo3
+    {
+        public Film MyForm { get; set; } = new Film();
+
+        public void Submit()
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(MyForm));
+        }
+    }
+}
---
+++ DemoBlazorWASM/Pages/Demo3/Film.cs
@@ -0,0 +1,22 @@
+﻿using DataAnnotationsExtensions;
+using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoBlazorWASM.Pages.Demo3
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Le titre est obligatoire pour un film espece d'abruti")]
+        [MinLength(1)]
+        public string Title { get; set; }
+        [Range(1970, 2025)]
+        public int ReleaseYear { get; set; }
+        [Required]
+        public string RealistorFirstname { get; set; }
+        [Required]
+        public string RealistorLastname { get; set; }
+    }
+
+    
+}
